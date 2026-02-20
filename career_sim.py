5866f20fd5238890f519fc0b1adddd264501014a
sidhaarrtthh <sidhalt7846@gmail.com>
2026-02-20 19:11:17 +0530

    Alpha 1.1 - Improved transfer system, player agency added

--- career_sim.py
+++ career_sim.py
@@ -1,15 +1,44 @@
 import random
 import time
-academy_club  = ['Northwells Academy' , 'Manchester Young Boys' , 'Northern Athletic Academy' , 'Southwells Academy']
-local_club = ['Seaside FC' , 'Modern Athletics FC' , 'Newtown FC']
-pro_club = ['Northwells FC' , 'Southwells FC', 'Northern Athletic FC' , 'Bluco FC']
+academy_club  = ['Northwells Academy' , 'Manchester Young Boys' , 'Northern Athletic Academy' , 'Southwells Academy' , 'Westside Youngs' , 'Flames Academy' ]
+local_club = ['Seaside FC' , 'Modern Athletics FC' , 'Newtown FC', 'Greenhills FC']
+pro_club = ['Northwells FC' , 'Southwells FC', 'Northern Athletic FC' , 'Bluco FC' , ]
 prem_club = ['Superstars FC' , 'Premier XI' , 'Westside FC' , 'Toppers FC']
+agency = ['Nextstep Talent Management' , 'Allround Talent Management' , 'Superstarts Contracts' , 'Lightning Talent Management']
 
 match_time = random.randint(1,95)
 
 
 
-print('Footballer: Academy to Legend | Alpha 1.0')
+def play_match(player):
+    match_goals = 0
+    match_assists = 0
+    
+    goal_chance = random.randint(1,100)
+    assist_chance = random.randint(1,100)
+
+    if goal_chance <= player['shooting']:
+        player['goals'] += 1
+        match_goals += 1
+        print("You scored a GOAL!")
+
+    if assist_chance <= player['passing']:
+        player['assist'] += 1
+        match_assists += 1
+        print("You made an ASSIST!")
+
+    rating = 6 + (match_goals * 1.5) + (match_assists * 1)
+
+    if rating > 10:
+        rating = 10
+
+    print("Match Rating:", round(rating,1))
+
+
+
+
+
+print('Footballer: Academy to Legend | Alpha 1.1')
 
 print('Welcome to Footballer: Academy to Legend')
 time.sleep(2)
@@ -31,7 +60,8 @@ player = {'name' : pname ,
           'handling' : 50 ,
           'club' : random.choice(academy_club),
           'goals' : 0,
-          'assist' : 0}
+          'assist' : 0,
+          'Agency' : 'No Agent'}
                         
 print('player details : ' , player) 
 month = 1
@@ -56,57 +86,28 @@ while True:
             player['shooting'] += 5
             print('Shooting Increased by 5 points!')
 
-            month += 1
+            
 
         elif ch ==2:
             player['passing'] += 5
             print('Passing Increased by 5 points!')
-            month += 1
+            
         
         elif ch == 3:
             player['handling'] += 5
             print('Handling stats increased by 5')
-            month += 1
+            
         elif ch == 4:
             player['pace'] += 5
             print('player pace increased by 5')
-            month += 1
+            
         else:
             print('Month Passed without any productive activity')
-            month += 1
+            
         if month== 3 or month== 6 or month==9 or month==12:
-            print('\nMatch Day!')
-            time.sleep(1)
-            print('Playing match....')
-            time.sleep(3)
-            match_goals = 0
-            match_assists = 0
-            goal_chance = random.randint(1,100)
-            assist_chance = random.randint(1,100)
-            if goal_chance <= player['shooting']:
-                player['goals'] += 1
-                match_goals += 1
-                print("You scored a GOAL!")
+            play_match(player)
+        month+=1
 
-            if assist_chance <= player['passing']:
-                player['assist'] += 1
-                match_assists += 1
-                print("You made an ASSIST!")
-            # MATCH RATING SYSTEM
-            rating = 6 + (match_goals * 1.5) + (match_assists * 1)
-
-            if rating > 10:
-                rating = 10
-
-            print("Match Rating:", round(rating, 1), "/ 10")
-            if rating >= 9:
-                print('Man of the match!')
-
-            elif rating >=7:
-                print('Good game!')
-            elif rating ==6:
-                print('You barely had an impact.')
-            time.sleep(2)
 
 
 
@@ -124,24 +125,53 @@ while True:
 
     if current_club in academy_club:
         if goals >= 1:
-            player['club'] = random.choice(local_club)
-            print("Scouts were impressed! You have been signed by", player['club'])
-        else:
-            print("No scouts were interested in you. You decided to train at", player['club'], "for another season.")
+            print('Local Scouts have ten notice of you...')
+            time.sleep(2)
+            new_club = random.choice(local_club)
+            print("\nTRANSFER OFFER!")
+            print(new_club, "wants to sign you.")
+
+            choice = input("Do you accept? (y/n): ")
+
+            if choice.lower() == 'y':
+                player['club'] = new_club
+                print("You signed for", new_club)
+                agent = random.choice(agency)
+                print('you have also signed for a player agency! ' , agent)
+                player['Agency'] = agent
+            else:
+                print("You decided to stay at", player['club'])
 
     elif current_club in local_club:
         if goals >= 2:
-            player['club'] = random.choice(pro_club)
-            print(player['club'] , 'signs' , pname , 'after an impressive season.' , " You joined", player['club'])
-        else:
-            print("Contract extended at", player['club'], "for another season.")
+            print('Message from agent : Many clubs are asking about you...An offer is expected soon.')
+            time.sleep(2)
+            new_club = random.choice(pro_club)
+            print("\nTRANSFER OFFER!")
+            print(new_club, "wants to sign you.")
+
+            choice = input("Do you accept? (y/n): ")
+
+            if choice.lower() == 'y':
+                player['club'] = new_club
+                print("You signed for", new_club)
+            else:
+                print("You decided to stay at", player['club'])
 
     elif current_club in pro_club:
-        if goals >= 5:
-            player['club'] = random.choice(prem_club)
-            print("SHOCKING MOVE! " , player['club'], ' signs ' , pname , 'for a HISTORIC transfer fee' ,  "You signed for", player['club'])
-        else:
-            print("Contracted Extended at", player['club'], "for another season.")
+        if goals >= 4:
+            print("Message from agent : A Prem League club is interested! Don't let this oppurtinity slide.")
+            new_club = random.choice(prem_club)
+            print("\nTRANSFER OFFER!")
+            print(new_club, "wants to sign you.")
+
+            choice = input("Do you accept? (y/n): ")
+
+            if choice.lower() == 'y':
+                player['club'] = new_club
+                print("You signed for", new_club)
+            else:
+                print("You decided to stay at", player['club'])
 
     # NOW reset season stats
     player['goals'] = 0
@@ -169,3 +199,5 @@ while True:
 
 
 print('Thank You for playing Footabller : Academy to Legend.')
+                
+    
